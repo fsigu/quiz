033efc2ddec098aeb8632acd02c4ac13cd95d131
Francisco <fsigugom@ucm.es>
2015-08-17 13:52:21 +0200

    Ejercicio Obligatorio Modulo9: Parte2

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,4 @@
-// Definicion del modelo de Quiz con validación
+// Definicion del modelo de Comment con validación
 
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define(
@@ -11,6 +11,15 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.BOOLEAN,
 			defaultValue: false
 		}
+		},
+		{
+		classMethods: {
+			countUnpublished: function () {
+			return this.count({ where: { publicado: false } });
+		},
+		countCommentedQuizes: function () {
+			return this.aggregate('QuizId', 'count', { distinct: true });
 		}
-	);
-}
+		}
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,17 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var statisticsController = require('../controllers/statistics_controller');
 
 /*  Página de entrada (GET homepage) */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
+// Definición de ruta para las estadísticas
+router.get('/quizes/statistics', statisticsController.calculate, statisticsController.show);
+
+// Definición de rutas de /author
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz', errors: [] });
 });
